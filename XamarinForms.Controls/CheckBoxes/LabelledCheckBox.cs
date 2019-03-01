795657d7cf7bf8dc97713454d1cd354cefd1ab1e
Laurent Greyling <L.Greyling@nipo.com>
2019-03-01 15:23:30 +0100

    try to create checkbox with a label

---
+++ XamarinForms.Controls/CheckBoxes/LabelledCheckBox.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms.Controls.CheckBoxes
+{
+    public class LabelledCheckBox : ContentView
+    {
+        public string LabelText { get; set; }
+
+        public double LabelFontSize { get; set; }
+
+        public string LabelFontFamily { get; set; }
+
+        public LabelledCheckBox()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new CheckBox(),
+                    new Label {
+                        Text = LabelText,
+                        FontFamily = LabelFontFamily,
+                        FontSize = LabelFontSize
+                    }                    
+                }
+            };
+        }
+    }
+}
